a05e74c53443d54ca2f9564d58999352d70f9614
EmFresh <emory.wynn@uoit.net>
2020-06-09 04:59:12 -0400

    Mucho progress

--- Assets/_Scripts/HtmlVideoFinder.cs
+++ Assets/_Scripts/HtmlVideoFinder.cs
@@ -1,29 +1,82 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using ScrapySharp.Extensions;
-using ScrapySharp.Network;
+using System.Collections.ObjectModel;
+//using HtmlAgilityPack;
+//using HTML_Finder;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+//using OpenQA.Selenium.
+//using ScrapySharp.Extensions;
+//using ScrapySharp.Network;
 using UnityEngine;
 
 public class HtmlVideoFinder : MonoBehaviour
 {
-    public string url;
-    // Start is called before the first frame update
-    void Start()
+    static ChromeDriver driver;
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
     {
-        ScrapingBrowser browser = new ScrapingBrowser();
-        // browser.NavigateToPage();
-        var browser2 = browser.NavigateToPage(new Uri(url));
-
-        var list = browser2.Html.CssSelect("video");
-
-        foreach (var node in list)
-         CreatePopups.SendPopup(node);
+        if (driver == null)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--headless");
+            driver = new ChromeDriver(options);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void URLStringFinder(string url)
     {
+        driver.Navigate().GoToUrl(@"https://9anime.ru/watch/welcome-to-demon-school-iruma-kun-dub.1k0x/jvv8333");
+        ReadOnlyCollection<IWebElement> ele = driver.FindElements(By.TagName("iframe"));
+        // driver.Capabilities.BrowserName;
+
+        string str;
+        // OpenQA.Selenium.
+        foreach (var element in ele)
+        {
+            str = element.GetAttribute("src");
+            print(str);
+            try
+            {
+                var tmp = element.FindElement(By.TagName("source"));
+                print("step 1");
+                print(tmp.GetAttribute("src"));
+                print("step 2");
+            }
+            catch
+            {
+                print("this did not work");
+            }
+
+            if (str.Contains("mp4upload"))
+            {
+                driver.Navigate().GoToUrl(str);
+
+                try
+                {
+                    print(driver.FindElement(By.TagName("source")).GetAttribute("src"));
+                }
+                catch
+                {
+                    print("could not find video");
+                    //<div class="plyr__video-wrapper"><video poster="https://www12.mp4upload.com/i/01471/fyrrthn00d03.jpg" playsinline=""><source src="https://www12.mp4upload.com:282/d/q6xqbsjvz3b4quuowowryk2sknisf4o6yz7ujpy7bo5yo5m2bnaj4spm/video.mp4" type="video/mp4"></video></div>
+                }
+                break;
+            }
+        }
 
     }
+
+    /// <summary>
+    /// Callback sent to all game objects before the application is quit.
+    /// </summary>
+    void OnApplicationQuit()
+    {
+        driver.Quit();
+        //        driver.Close();
+    }
+
 }
